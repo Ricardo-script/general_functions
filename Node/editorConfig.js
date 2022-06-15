b2f216427e4926d847c079fc964120b2866cf607
Ricardo Teixeira <64698638+Ricardo-script@users.noreply.github.com>
2022-06-15 16:29:31 -0300

    insert editorConfig

---
+++ Node/editorConfig.js
@@ -0,0 +1,9 @@
+root = true
+
+[*]
+indent_style = space // or tab
+indent_size = 4
+end_of_line = lf  //Novas linhas no estilo Unix com uma nova linha terminando cada arquivo
+charset = utf-8
+trim_trailing_whitespace = true
+insert_final_newline = true
