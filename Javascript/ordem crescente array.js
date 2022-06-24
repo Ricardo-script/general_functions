7056bbd96f7d7d09f0ff912ee917eacd4f58d702
Ricardo Teixeira <64698638+Ricardo-script@users.noreply.github.com>
2022-06-24 11:07:12 -0300

    array crescente e decrescente

--- Javascript/ordem crescente array.js
+++ Javascript/ordem crescente array.js
@@ -1,3 +1,44 @@
+	
+/* 
+	A função passada para sort recebe dois parâmetros que representam pares de elementos, isso porque toda comparação envolve um par de elementos. 
+	A regra é a seguinte: com o critério selecionado, se o valor retornado for 0 não há alteração a ser feita, se o valor retornado for positivo, 
+	b deve vir antes de a, se o valor for negativo, a deve vir antes de b.
+	
+*/	
+	
+const dados = [
+	{nome: 'Ricardo', idade: 34, cargo: 'Front-end' },
+	{nome: 'Vinicius',idade: 24, cargo: 'Back-end' },
+	{nome: 'Felipe',  idade: 27, cargo:'Full-stack'} 
+];
+
+const crescente = () =>{
+	const ordemCrescente = [];
+	dados.sort((a,b) => a.idade - b.idade);
+	dados.forEach( item => {
+		ordemCrescente.push(item);
+	});
+
+	console.log(ordemCrescente);
+}
+
+//Para obtermos uma ordem decrescente é só inverter a subtração b - a:
+const decrescente = () =>{
+	const ordemDecrescente = [];
+	dados.sort((a,b) => b.idade - a.idade);
+	dados.forEach( item => {
+		ordemDecrescente.push(item);
+	});
+
+	console.log(ordemDecrescente);
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------------------------
+
+
+//Exemplo: 2
+	
 var pessoas = [
     {
       nome: "Matheus",
