c3fc1fcbae0dbf0aa5cbd02333f403cd9f7c5403
Ricardo Teixeira <64698638+Ricardo-script@users.noreply.github.com>
2022-08-02 11:00:19 -0300

    mascaras para inputs

--- Javascript/mascaras inputs.js
+++ Javascript/mascaras inputs.js
@@ -1,30 +1,62 @@
 // --Telefone:--------------------------------------------------------------------
-const formatarTel = (value) => {
-	const formatted = value.replace(/^(\d{2})(\d{5})(\d{4}).*/,"($1) $2-$3");
-	console.log(formatted);
-	return formatted;
+function phoneMask(v) {
+
+	let mask = v.replace(/\D/g, "");
+	mask = mask.replace(/^0/, "");
+
+	if (mask.length > 11) {
+		mask = mask.replace(/^(\d\d)(\d{5})(\d{4}).*/, "($1) $2-$3");
+	} else if (mask.length > 7) {
+		mask = mask.replace(/^(\d\d)(\d{5})(\d{0,4}).*/, "($1) $2-$3");
+	} else if (mask.length > 2) {
+		mask = mask.replace(/^(\d\d)(\d{0,5})/, "($1) $2");
+	} else if (v.trim() !== "") {
+		mask = mask.replace(/^(\d*)/, "($1");
+	}
+	console.log(mask)
+	return mask;
 }
-formatarTel('11912345678');
-//saída: (11) 91234-5678
+
+phoneMask('11975848563');
+//saída: (11) 97584-8563
 //---------------------------------------------------------------------------------
 
 
 
 //--CPF----------------------------------------------------------------------------
-var cpf = '69845712398'
-cpf = cpf.replace(/[^\d]/g, "");
-var formataCPF = cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
-//saída: '698.457.123-98'
+    function cpfMask(v){
+        v=v.replace(/\D/g,"")                    //Remove tudo o que não é dígito
+        v=v.replace(/(\d{3})(\d)/,"$1.$2")       //Coloca um ponto entre o terceiro e o quarto dígitos
+        v=v.replace(/(\d{3})(\d)/,"$1.$2")       //Coloca um ponto entre o terceiro e o quarto dígitos
+                                                //de novo (para o segundo bloco de números)
+        v=v.replace(/(\d{3})(\d{1,2})$/,"$1-$2") //Coloca um hífen entre o terceiro e o quarto dígitos
+        console.log(v)
+        return v
+    }
 
-//ou: exemplo com regex com tolerância a nulos ou undefined
-function formatCpf(text) {
-    const badchars = /[^\d]/g
-    const mask = /(\d{3})(\d{3})(\d{3})(\d{2})/
-    const cpf = new String(text).replace(badchars, "");
-    return cpf.replace(mask, "$1.$2.$3-$4");
- }
+    cpfMask('65161174030');
+    //saída 651.611.740-30
 //---------------------------------------------------------------------------------
 
+
+
+
+//--CNPJ-------------------------------------------------------------------------------------------------------------
+function cnpjMask(v){
+		v=v.replace(/\D/g,"")                           //Remove tudo o que não é dígito
+		v=v.replace(/^(\d{2})(\d)/,"$1.$2")             //Coloca ponto entre o segundo e o terceiro dígitos
+		v=v.replace(/^(\d{2})\.(\d{3})(\d)/,"$1.$2.$3") //Coloca ponto entre o quinto e o sexto dígitos
+		v=v.replace(/\.(\d{3})(\d)/,".$1/$2")           //Coloca uma barra entre o oitavo e o nono dígitos
+		v=v.replace(/(\d{4})(\d)/,"$1-$2")  
+
+		console.log(v);          //Coloca um hífen depois do bloco de quatro dígitos
+		return v;
+	}
+
+	cnpjMask('68527736000156');
+	//saída: 68.527.736/0001-56
+
+//------------------------------------------------------------------------------------------------------------------
 
 
 //--Email--------------------------------------------------------------------------
@@ -45,7 +77,7 @@ console.log(validateEmail(emailAdress));
 
 
 
-//CEP------------------------------------------------------------------------------
+//--CEP------------------------------------------------------------------------------
 // /\D/g pesquisa global por caracteres que não são dígitos:
 function formatarCep(cep) {
 	const badchars = cep.replace(/\D/g, "") 
@@ -54,4 +86,20 @@ function formatarCep(cep) {
 }
 
 formatarCep('06421000');
+//---------------------------------------------------------------------------------
+
+
+//--Remover acentos----------------------------------------------------------------
+function removeAcento (text)
+{       
+    text = text.toLowerCase();   
+	text = text.replace(new RegExp('[`"^]','gi'), '');
+    text = text.replace(new RegExp('[ÁÀÂÃ]','gi'), 'a');
+    text = text.replace(new RegExp('[ÉÈÊ]','gi'), 'e');
+    text = text.replace(new RegExp('[ÍÌÎ]','gi'), 'i');
+    text = text.replace(new RegExp('[ÓÒÔÕ]','gi'), 'o');
+    text = text.replace(new RegExp('[ÚÙÛ]','gi'), 'u');
+    text = text.replace(new RegExp('[Ç]','gi'), 'c');
+    return text;                 
+}
 //---------------------------------------------------------------------------------
