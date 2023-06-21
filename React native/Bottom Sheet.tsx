876b94b1ea3ff54a2ace56d27cd1e62f2c264c1f
Ricardo-loadcode <teixeira.rcd@gmail.com>
2023-06-21 00:56:45 -0300

    Bottom Sheet

--- React native/Bottom Sheet.tsx
+++ React native/Bottom Sheet.tsx
@@ -15,7 +15,7 @@ const Home = () => { // precisa inserir o export default no final para usar o ge
     const BottomSheetRef = useRef<BottomSheet>(null);
 	
 	return(														      					
-		<Button title='mostrar' onPress={() => { bottomSheetRef.current?.expand() }} /> 
+		<Button title='mostrar' onPress={() => { bottomSheetRef.current?.expand() }} /> // responsavel para expandir o BottomSheet se clicado
 		<BottomSheet ref={bottomSheetRef} index={1} snapPoints={[20, '60%']}>// snapPoints: recebe o tamanho inicial e final index: 1 aberto e 0 fechado 
 			<View>
 				<Text>Teste</Text>
