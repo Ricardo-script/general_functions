ffe2f82003de9b1a81e703813863c177af7ea705
Ricardo Teixeira <64698638+Ricardo-script@users.noreply.github.com>
2023-09-11 09:09:23 -0300

    Update requisição com hooks.tsx

--- Typescript/requisição com hooks.tsx
+++ Typescript/requisição com hooks.tsx
@@ -76,3 +76,77 @@ export default function Home(): JSX.Element {
 
 //POST:
 
+import { useState } from "react"
+import axios from "axios"
+
+type UsePostReturnType<T> = {
+    executePost: (postData: T) => Promise<void>,
+    loading: boolean,
+    error: string
+}
+
+export function usePost<T>(url: string): UsePostReturnType<T> {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState('');
+
+    const executePost = async (postData: T) => {
+        setLoading(true);
+        console.log('postData', postData)
+        try {
+            const response = await axios.post(url, postData);
+            console.log('Response:', response);
+        } catch (error) {
+            console.error('Error posting data:', error);
+            //setError(error)
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    return { executePost, loading, error };
+}
+
+// arquivo de chamada:
+
+'use client'
+
+import { useRef } from "react";
+import { usePost } from "./hooks";
+
+type PostType = {
+    nome: string
+    email: string
+    password: string
+}
+
+export default function Home(): JSX.Element {
+
+    const formRef = useRef<HTMLFormElement>(null);
+
+    const {
+        executePost,
+        loading,
+        error
+    } = usePost<PostType>('https://jsonplaceholder.typicode.com/posts');
+
+    const sendData = async () => {
+        const postData: PostType = {
+            nome: formRef.current?.nome?.value,
+            email: formRef.current?.email?.value,
+            password: formRef.current?.password?.value
+        };
+
+        await executePost(postData);
+    }
+
+    return (
+        <section>
+            <form ref={formRef}>
+                <input type="text" name="nome" />
+                <input type="email" name="email" />
+                <input type="password" name="password" />
+            </form>
+            <button onClick={sendData}>Send</button>
+        </section>
+    );
+}
