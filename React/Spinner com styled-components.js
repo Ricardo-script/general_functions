03182c7b5c64e35a795ae4929f19ca6a7c076db2
Ricardo Teixeira <64698638+Ricardo-script@users.noreply.github.com>
2024-09-16 11:46:37 -0300

    Update Spinner com styled-components.js

--- React/Spinner com styled-components.js
+++ React/Spinner com styled-components.js
@@ -107,3 +107,27 @@ export const SpinnerDiv = styled.div`
         animation-delay: 0s;
     }
 `;
+
+// opção 2: ----------------------------------------------------------------------------------------
+
+import styled, { keyframes } from 'styled-components';
+
+const rotate = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+export const Spinner = styled.div`
+	border: 2px solid rgba(255, 255, 255, 0.3);
+	border-top: 2px solid white;
+	border-radius: 50%;
+	width: 16px;
+	height: 16px;
+	animation: ${rotate} 0.8s linear infinite;
+	margin-right: 8px;
+`;
+
