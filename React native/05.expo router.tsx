7fdd337b37ffba585b6ed8c8c11138d0b2afdc7a
Ricardo Teixeira <64698638+Ricardo-script@users.noreply.github.com>
2024-10-01 12:54:35 -0300

    Update 05.expo router.tsx

--- React native/05.expo router.tsx
+++ React native/05.expo router.tsx
@@ -1,10 +1,17 @@
-## expo routes
+## create app
 
-- Criar para src, apagar tudo de dentro de app
+npx create-expo-app
+
+//----------------------------------------------------------------------------------------------------
+
+- Criar para src, apagar tudo de dentro de app e arrastar para dentro de src
 - Ao criar a pasta src deve mudar o caminho da pasta dentro de tsconfig.json e na propriedade compilerOptions
 
-"paths": {
-"@/_": ["./src/_"]
-}
+"compilerOptions": {
+     "strict": true,
+      "paths": {
+      "@/*": ["./src/*"]
+    }
+},
 
-- criar um arquivo chamado '\_layout.tsx' dentro de app que sera o ponto de entrada da aplicação
+- criar um arquivo chamado '_layout.tsx' dentro de app que sera o ponto de entrada da aplicação
