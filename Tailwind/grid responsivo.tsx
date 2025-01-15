50f1acf2136de1a1e2e2c331fef61231414918d9
Ricardo Teixeira <64698638+Ricardo-script@users.noreply.github.com>
2025-01-15 13:16:02 -0300

    Create grid responsivo.tsx

---
+++ Tailwind/grid responsivo.tsx
@@ -0,0 +1,19 @@
+<div className="grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-4">
+    <div className="bg-white/10 rounded">Playlist</div>
+    <div className="bg-white/10 rounded">Playlist</div>
+    <div className="bg-white/10 rounded">Playlist</div>
+    <div className="bg-white/10 rounded">Playlist</div>
+    <div className="bg-white/10 rounded">Playlist</div>
+    <div className="bg-white/10 rounded">Playlist</div>
+</div>
+
+//equivalente ao css:
+
+/* 
+  .cards {
+      display: grid;
+      grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+      grid-gap: 16px;
+  }
+
+*/ 
