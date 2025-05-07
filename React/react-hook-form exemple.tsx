74bc30eee576b26c473fede51d30deb8204df7bc
Ricardo Teixeira <64698638+Ricardo-script@users.noreply.github.com>
2025-05-07 15:25:23 -0300

    Update react-hook-form exemple.tsx

--- React/react-hook-form exemple.tsx
+++ React/react-hook-form exemple.tsx
@@ -68,7 +68,7 @@ export default function App() {
     );
 }
 
-//********* ************** *************** **************** ***************/
+//********* ************** *************** **************** *************** ************** **********/
 
 
 import { useFormContext } from "react-hook-form";
@@ -87,6 +87,128 @@ export function Input(props: InputProps) {
             {...register(props.name)}
             {...props}
         />
+    );
+}
+
+//**************************************************************************************************/
+/*Mais exemplos, habilitar botão somenmte quando formulario atender todos os requisitos com isValid,
+  auto preencher valores nos inputs que vem de fora com reset()
+*/
+
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect } from "react";
+
+const createUserFormSchema = z.object({
+    name: z.string().nonempty("O campo nome é obrigatório"),
+    email: z
+        .string()
+        .nonempty("O campo e-mail é obrigatório")
+        .email("Formato de e-mail inválido"),
+    password: z
+        .string()
+        .nonempty("O campo de senha é obrigatório")
+        .min(6, "O campo de senha deve ter pelo menos 6 caracteres"),
+});
+
+type CreateUserTypes = z.infer<typeof createUserFormSchema>;
+
+export default function App() {
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors, isValid }, // isValid retorna booleano que verifica se atendeu todos os requisitos (habilitar botão)
+    } = useForm<CreateUserTypes>({
+        resolver: zodResolver(createUserFormSchema),
+    });
+
+    const createUser = (data: CreateUserTypes) => {
+        console.log(data);
+    };
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+            const apiResponse: CreateUserTypes = {
+                name: "João da Silva",
+                email: "joao.silva@email.com",
+                password: "123456",
+            };
+
+            reset(apiResponse);
+        };
+
+        fetchUserData();
+    }, [reset]);
+
+    return (
+        <main className="p-4">
+            <form
+                onSubmit={handleSubmit(createUser)}
+                className="w-[400px] flex flex-col gap-2"
+            >
+                <div className="flex flex-col">
+                    <label className="text-sm">Nome:</label>
+                    <input
+                        type="text"
+                        className="w-full h-8 border border-zinc-300 outline-none px-1 rounded-md"
+                        {...register("name")}
+                    />
+                    {errors.name && (
+                        <span className="text-xs text-red-500">
+                            {errors.name.message}
+                        </span>
+                    )}
+                </div>
+                <div className="flex flex-col gap-1">
+                    <label className="text-sm">Email:</label>
+                    <input
+                        type="email"
+                        className="w-full h-8 border border-zinc-300 outline-none px-1 rounded-md"
+                        {...register("email")}
+                    />
+                    {errors.email && (
+                        <span className="text-xs text-red-500">
+                            {errors.email.message}
+                        </span>
+                    )}
+                </div>
+                <div className="flex flex-col gap-1">
+                    <label className="text-sm">Password:</label>
+                    <input
+                        type="password"
+                        className="w-full h-8 border border-zinc-300 outline-none px-1 rounded-md"
+                        {...register("password")}
+                    />
+                    {errors.password && (
+                        <span className="text-xs text-red-500">
+                            {errors.password.message}
+                        </span>
+                    )}
+                </div>
+                <button
+                    disabled={!isValid}
+                    className="bg-emerald-500 w-full h-8 text-white rounded-sm cursor-pointer active:relative active:top-[1px] disabled:opacity-50 disabled:pointer-events-none select-none"
+                    type="submit"
+                >
+                    Enviar
+                </button>
+                <button
+                    className="bg-emerald-500 w-full h-8 text-white rounded-sm cursor-pointer active:relative active:top-[1px]"
+                    type="button"
+                    onClick={() =>
+                        reset({
+                            name: "",
+                            email: "",
+                            password: "",
+                        })
+                    }
+                >
+                    Limpar
+                </button>
+            </form>
+        </main>
     );
 }
 
